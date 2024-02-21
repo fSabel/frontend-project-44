4ac79c65b994b79779b9434dd894fcf149fa8533
Philipp <sabelnickov.filipp@yandex.ru>
2024-02-22 01:18:20 +0500

    update check() of brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,33 +1,79 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
+// Приветствие, начало игры
 console.log('Welcome to the Brain Games!');
 const name = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${name}!`);
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
+// Реализуем случайное число
 const getRandomNumber = (min, max) => {
     min = Math.ceil(min)
     max = Math.floor(max)
     return Math.floor(Math.random() * (max - min) + min)
 };
 
-const check = () => {
-    const randomNumber = getRandomNumber(0, 30);
-    console.log(`Question: ${randomNumber}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (randomNumber % 2 === 0 && answer.toLowerCase() === 'yes') {
-        return console.log('Correct!');
-    } else if (randomNumber % 2 !== 0 && answer.toLowerCase() === 'no') {
-        return console.log('Correct!');
-    } else {
-        const firstStr = "'yes' is wrong answer ;(. Correct answer was 'no'.\n";
-        const secondStr = `Let's try again, ${name}!`;
-        return console.log(`${firstStr}${secondStr}`);
-    }
-};
+// Проверка на корректность ответа
+// const check = () => {
+//     let correctSum = 0;
+//     let randomNumber = getRandomNumber(1, 30);
+//     console.log(`Question: ${randomNumber}`);
+//     let answer = readlineSync.question('Your answer: ');
+//     if (randomNumber % 2 === 0) {
+//         if (answer.toLowerCase() === 'yes') {
+//             console.log('Correct!');
+//             correctSum += 1;
+//             randomNumber = getRandomNumber(1, 30);
+//             console.log(`Question: ${randomNumber}`);
+//             answer = readlineSync.question('Your answer: ');
+//         }
+//         if (answer.toLowerCase() === 'yes') {
+//             console.log('Correct!');
+//             correctSum += 1;
+//             randomNumber = getRandomNumber(1, 30);
+//             console.log(`Question: ${randomNumber}`);
+//             answer = readlineSync.question('Your answer: ');
+//         }
+//         if (answer.toLowerCase() === 'yes') {
+//             console.log('Correct!');
+//             correctSum += 1;
+//             randomNumber = getRandomNumber(1, 30);
+//             console.log(`Question: ${randomNumber}`);
+//             answer = readlineSync.question('Your answer: ');
+//         }
+//     } else if (randomNumber % 2 !== 0) {
+//         if (answer.toLowerCase() === 'no') {
+//             console.log('Correct!');
+//             correctSum += 1;
+//             randomNumber = getRandomNumber(1, 30);
+//             console.log(`Question: ${randomNumber}`);
+//             answer = readlineSync.question('Your answer: ');
+//         }
+//         if (answer.toLowerCase() === 'no') {
+//             console.log('Correct!');
+//             correctSum += 1;
+//             randomNumber = getRandomNumber(1, 30);
+//             console.log(`Question: ${randomNumber}`);
+//             answer = readlineSync.question('Your answer: ');
+//         }
+//         if (answer.toLowerCase() === 'no') {
+//             console.log('Correct!');
+//             correctSum += 1;
+//             randomNumber = getRandomNumber(1, 30);
+//             console.log(`Question: ${randomNumber}`);
+//             answer = readlineSync.question('Your answer: ');
+//         }
+//     } else {
+//         const firstStr = "'yes' is wrong answer ;(. Correct answer was 'no'.\n";
+//         const secondStr = `Let's try again, ${name}!`;
+//         return console.log(`${firstStr}${secondStr}`);
+//     }
 
-console.log(check());
+//     if (correctSum === 3) {
+//         return console.log(`Congratulations, ${name}!`);
+//     }
+// };
 
+// console.log(check());
 
-// console.log(`Your answer: ${answer}`);
