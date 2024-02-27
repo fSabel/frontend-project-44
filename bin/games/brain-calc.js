497499386d64a0b61e5224ec7d1713ecbb817e29
Philipp <sabelnickov.filipp@yandex.ru>
2024-02-28 01:11:20 +0500

    add brain-calc.js

---
+++ bin/games/brain-calc.js
@@ -0,0 +1,18 @@
+// Генерация случайных математических операций
+// const ops=['+','-','*'];
+
+// let opindex = getRandomNumber(0, 2);
+// let operator = ops[opindex];
+// console.log(operator);
+
+
+// if (operator === '+') {
+//   result = num1 + num2;
+//   console.log(result);
+// } else if (operator === '-') {
+//   result = num1 - num2;
+//   console.log(result);
+// } else {
+//   result = num1 * num2;
+//   console.log(result);
+// }
--- src/index.js
+++ src/index.js
@@ -6,4 +6,3 @@ console.log('Welcome to the Brain Games!');
 const name = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${name}!`);
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
