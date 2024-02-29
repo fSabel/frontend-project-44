42ac39844622058266a49fdbbb3c6f1fc70f7acd
Philipp <sabelnickov.filipp@yandex.ru>
2024-02-29 23:11:36 +0500

    realization united logic at src/index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
-// Приветствие, начало игры
-console.log('Welcome to the Brain Games!');
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`);
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
+const unifiedLogic = () => {
+    // Приветствие, начало игры
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
+    let unitedRule = ruleForGame;
+    console.log(unitedRule);
+    let unitedCondition;
+
+    for (let correctly = 1; correctly <= 3; correctly += 1) {
+        unitedCondition = condition;
+        console.log(`Question: ${randomNumber}`);
+        let answer = readlineSync.question('Your answer: ');
+        if (unitedCondition1) {
+            console.log('Correct!');
+        } else {
+            return `${firstStr}${secondStr}`;
+        }
+    }
+    return `Congratulations, ${name}!`;
+
+};
+
+
+
