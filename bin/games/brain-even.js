af4fdf44ad5d9fabe9cb060ac6ad899f64266582
Philipp <sabelnickov.filipp@yandex.ru>
2024-03-27 19:08:43 +0500

    ref after linter

--- bin/games/brain-even.js
+++ bin/games/brain-even.js
@@ -1,12 +1,11 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import unifiedLogic from '/home/gringblade/frontend-project-44/src/index.js'
+import unifiedLogic from '/home/gringblade/frontend-project-44/src/index.js';
 
 // Реализуем случайное число
 const getRandomNumber = (min, max) => {
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    return Math.floor(Math.random() * (max - min) + min)
+  const minimum = Math.ceil(min);
+  const maximum = Math.floor(max);
+  return Math.floor(Math.random() * (maximum - minimum) + minimum);
 };
 
 // Правила игры
@@ -14,23 +13,22 @@ const rulesEven = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 // Проверка на корректность ответа
 const round = () => {
-    let answer;
-    let randomNumber = getRandomNumber(1, 30);
-    let question = `Question: ${randomNumber}`;
-    console.log(question);
-    if (randomNumber % 2 === 0) {
-        answer = 'yes';
-    } else {
-        answer = 'no';
-    }
-    
-    return answer;
-}
+  let answer;
+  const randomNumber = getRandomNumber(1, 30);
+  const question = `Question: ${randomNumber}`;
+  console.log(question);
+  if (randomNumber % 2 === 0) {
+    answer = 'yes';
+  } else {
+    answer = 'no';
+  }
+
+  return answer;
+};
 
 const check = () => {
-    const rules = `${rulesEven}`
-    return unifiedLogic(rules, round);
-}
+  const rules = `${rulesEven}`;
+  return unifiedLogic(rules, round);
+};
 
 console.log(check());
-
