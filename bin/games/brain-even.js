3890dcbc5341fc0d0a15322b503a3db730e3f44f
Philipp <sabelnickov.filipp@yandex.ru>
2024-04-11 01:04:05 +0500

    fix /bin/games/brain-even.js

--- bin/games/brain-even.js
+++ bin/games/brain-even.js
@@ -1,27 +1,26 @@
 #!/usr/bin/env node
-import { getRandomNumber, unifiedLogic } from '../../src/index.js';
+import runEngine from '../../src/index.js';
+import getRandomInRange from '../../src/utils.js';
 
 // Правила игры
 const rulesEven = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 // Проверка на корректность ответа
-const round = () => {
+const generateRound = () => {
   let answer;
-  const randomNumber = getRandomNumber(1, 30);
+  const randomNumber = getRandomInRange();
   const question = `Question: ${randomNumber}`;
-  console.log(question);
   if (randomNumber % 2 === 0) {
     answer = 'yes';
   } else {
     answer = 'no';
   }
 
-  return answer;
+  return [question, answer];
 };
 
 const check = () => {
   const rules = `${rulesEven}`;
-  return unifiedLogic(rules, round);
+  return runEngine(rules, generateRound);
 };
-
 console.log(check());
