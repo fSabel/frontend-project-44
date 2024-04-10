8946889601c1d5e4bec850b4021dedbcf6434114
Philipp <sabelnickov.filipp@yandex.ru>
2024-04-11 01:01:14 +0500

    add greeting at cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,11 @@
-const readlineSync = require('readline-sync');
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
 
-export default readlineSync;
+const greeting = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+
+  return `Hello, ${name}!`;
+};
+
+export default greeting;
