fa93afdd57ffe114860eb9f5231588cf17b1e71c
Philipp <sabelnickov.filipp@yandex.ru>
2024-04-22 22:55:14 +0500

    refactoring brain-gcd.js

--- bin/games/brain-gcd.js
+++ bin/games/brain-gcd.js
@@ -5,22 +5,16 @@ import getRandomInRange from '../../src/utils.js';
 // The rules of the game
 const rulesGcd = 'Find the greatest common divisor of given numbers.';
 
-const generateRound = () => {
+// Finding the largest common divisor
+const gcd = (num1, num2) => {
   let answer;
-  const randomNumber1 = getRandomInRange(1);
-  const randomNumber2 = getRandomInRange(1);
-
-  const question = `Question: ${randomNumber1} ${randomNumber2}`;
-
-  // Finding the largest common divisor
-  if (randomNumber1 >= randomNumber2 && randomNumber1 % randomNumber2 === 0) {
-    return [question, randomNumber2.toString()];
+  if (num1 >= num2 && num1 % num2 === 0) {
+    return num2.toString();
   }
-
-  if (randomNumber1 <= randomNumber2 && randomNumber2 % randomNumber1 === 0) {
-    return [question, randomNumber1.toString()];
+  if (num1 <= num2 && num2 % num1 === 0) {
+    return num1.toString();
   }
-  const arr = [randomNumber1, randomNumber2];
+  const arr = [num1, num2];
   if (arr[0] < arr[1]) {
     arr.reverse();
   }
@@ -29,14 +23,19 @@ const generateRound = () => {
     arr.push(res);
     if (res === 0) {
       answer = arr[i + 1];
-      return [question, answer.toString()];
+      return answer.toString();
     }
   }
-  return [question, answer.toString()];
+  return answer.toString();
 };
 
-const check = () => {
-  const rules = `${rulesGcd}`;
-  return runEngine(rules, generateRound);
+const generateRound = () => {
+  const randomNumber1 = getRandomInRange(1);
+  const randomNumber2 = getRandomInRange(1);
+
+  const question = `Question: ${randomNumber1} ${randomNumber2}`;
+  const answer = gcd(randomNumber1, randomNumber2);
+  return [question, answer];
 };
-console.log(check());
+
+console.log(runEngine(rulesGcd, generateRound));
