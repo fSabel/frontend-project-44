620d2356aad63116543e274854711725e921d93a
Philipp <sabelnickov.filipp@yandex.ru>
2024-05-06 23:45:06 +0500

    add import for brain-even

--- bin/games/brain-even.js
+++ bin/games/brain-even.js
@@ -1,19 +1,4 @@
 #!/usr/bin/env node
-import runEngine from '../../src/index.js';
-import getRandomInRange from '../../src/utils.js';
+import brain_even from '../../src/games/brain-even.js';
 
-// The rules of the game
-const rulesEven = 'Answer "yes" if the number is even, otherwise answer "no".';
-
-// Checking for the correctness of the response
-const isEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
-
-// Round generation
-const generateRound = () => {
-  const randomNumber = getRandomInRange();
-  const question = `Question: ${randomNumber}`;
-  const answer = isEven(randomNumber);
-  return [question, answer];
-};
-
-console.log(runEngine(rulesEven, generateRound));
+brain_even();
