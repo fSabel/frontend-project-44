6c7161ab9a94689be3058ce483d6b568c7d49ad5
Philipp <sabelnickov.filipp@yandex.ru>
2024-05-06 23:44:57 +0500

    add import for brain-calc

--- bin/games/brain-calc.js
+++ bin/games/brain-calc.js
@@ -1,34 +1,4 @@
 #!/usr/bin/env node
-import runEngine from '../../src/index.js';
-import getRandomInRange from '../../src/utils.js';
+import brain_calc from '../../src/games/brain-calc.js';
 
-// The rules of the game
-const rulesCalc = 'What is the result of the expression?';
-
-const getRandomOperator = () => {
-  const operators = ['+', '-', '*'];
-  return operators[getRandomInRange(0, operators.length - 1)];
-};
-
-// Calculator
-const calculation = (num1, num2, operator) => {
-  switch (operator) {
-    case '+': return num1 + num2;
-    case '-': return num1 - num2;
-    case '*': return num1 * num2;
-    default: throw new Error(`Invalid operator - ${operator})`);
-  }
-};
-
-// Round generation
-const generateRound = () => {
-  const num1 = getRandomInRange();
-  const num2 = getRandomInRange();
-  const operator = getRandomOperator();
-  const question = `Question: ${num1} ${operator} ${num2}`;
-  const answer = String(calculation(num1, num2, operator));
-
-  return [question, answer];
-};
-
-console.log(runEngine(rulesCalc, generateRound));
+brain_calc();
